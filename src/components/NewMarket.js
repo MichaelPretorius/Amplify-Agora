4c9d5f14b02ec230c76836fdf887474d706d22ce
Michael Pretorius <michael.pretorius@younglings.africa>
2020-03-13 09:40:22 +0200

    adding select tegs for creating market

--- src/components/NewMarket.js
+++ src/components/NewMarket.js
@@ -7,15 +7,27 @@ import { UserContext } from '../App';
 const NewMarket = () => {
   const [addMarketDialog, setaddMarketDialog] = useState(false);
   const [name, setname] = useState('');
+  const [selectedTags, setselectedTags] = useState([]);
+  const [options, setoptions] = useState([]);
 
+  const tags = ['Arts', 'Technology', 'Crafts', 'Entertainment', 'Web Dev'];
   const { user } = useContext(UserContext);
 
   const handleAddMarket = async () => {
     try {
       setaddMarketDialog(false);
-      const res = await API.graphql(graphqlOperation(createMarket, { input: { name, owner: user.username }}));
+      const res = await API.graphql(
+				graphqlOperation(createMarket, {
+					input: {
+						name,
+            owner: user.username,
+            tags: selectedTags
+					},
+				}),
+      );
       console.log(`Created market: id ${res.data.createMarket.id}`);
       setname('');
+      setselectedTags([]);
     } catch (err) {
       console.log('Error adding new market: ', err)
       Notification.error({
@@ -23,6 +35,14 @@ const NewMarket = () => {
         message: `${err.message || "Error adding market"}`
       })
     }
+  }
+
+  const handleFilterTags = query => {
+    const options = tags
+      .map(tag => ({ value: tag, label: tag}))
+      .filter(tag => tag.label.toLowerCase().includes(query.toLowerCase()))
+
+    setoptions(options);
   }
 
   return (
@@ -43,30 +63,42 @@ const NewMarket = () => {
 				title='Create New Market'
 				visible={addMarketDialog}
 				onCancel={() => setaddMarketDialog(false)}
-        size="large"
-        customClass="dialog"
+				size='large'
+				customClass='dialog'
 			>
-        <Dialog.Body>
-          <Form labelPosition="top">
-            <Form.Item label="Add Market Name">
-              <Input
-                placeholder="Market Name"
-                trim={true}
-                onChange={name => setname(name)}
-                value={name}
-              />
-            </Form.Item>
-          </Form>
-        </Dialog.Body>
-        <Dialog.Footer>
-          <Button onClick={() => setaddMarketDialog(false)}>
-            Cancel
-          </Button>
-          <Button type="primary" disabled={!name} onClick={handleAddMarket}>
-            Add
-          </Button>
-        </Dialog.Footer>
-      </Dialog>
+				<Dialog.Body>
+					<Form labelPosition='top'>
+						<Form.Item label='Add Market Name'>
+							<Input
+								placeholder='Market Name'
+								trim={true}
+								onChange={name => setname(name)}
+								value={name}
+							/>
+						</Form.Item>
+						<Form.Item labe='Add Tags'>
+							<Select
+								multiple
+								filterable
+								remote
+								placeholder='Market Tags'
+								onChange={tags => setselectedTags(tags)}
+								remoteMethod={handleFilterTags}
+							>
+								{options.map(({value, label}) => (
+									<Select.Option key={value} label={label} value={value} />
+								))}
+							</Select>
+						</Form.Item>
+					</Form>
+				</Dialog.Body>
+				<Dialog.Footer>
+					<Button onClick={() => setaddMarketDialog(false)}>Cancel</Button>
+					<Button type='primary' disabled={!name} onClick={handleAddMarket}>
+						Add
+					</Button>
+				</Dialog.Footer>
+			</Dialog>
 		</>
 	);
 }
