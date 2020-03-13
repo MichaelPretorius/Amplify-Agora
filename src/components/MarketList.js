990084e88c42f9005185e5058bf6b1331366b712
Michael Pretorius <michael.pretorius@younglings.africa>
2020-03-13 13:40:34 +0200

    adding onMarketCreate sub

--- src/components/MarketList.js
+++ src/components/MarketList.js
@@ -3,63 +3,84 @@ import { graphqlOperation } from 'aws-amplify';
 import { Connect } from 'aws-amplify-react';
 import { Link } from 'react-router-dom'
 import { listMarkets } from '../graphql/queries';
+import { onCreateMarket } from '../graphql/subscriptions';
 import { Loading, Card, Icon, Tag } from 'element-react';
 import Error from './Error';
 
 const MarketList = () => {
+  const onNewMarket = (prevQuery, newData) => {
+    let updatedQuery = { ...prevQuery };
+    updatedQuery.listMarkets.items = [
+			newData.onCreateMarket,
+			...prevQuery.listMarkets.items,
+    ];
+    return updatedQuery;
+  }
+
   return (
-    <Connect query={graphqlOperation(listMarkets)}>
-      {({data, loading, errors}) => {
-        if (errors.length > 0) return <Error errors={errors} />
+		<Connect
+			query={graphqlOperation(listMarkets)}
+			subscription={graphqlOperation(onCreateMarket)}
+      onSubscriptionMsg={onNewMarket}
+		>
+			{({data, loading, errors}) => {
+				if (errors.length > 0) return <Error errors={errors} />;
 
-        if (loading || !data.listMarkets ) return <Loading fullscreen />
-        console.log(data)
+				if (loading || !data.listMarkets) return <Loading fullscreen />;
 
-        return (
-          <>
-            <h2 className="header">
-              <img src="https://icon.now.sh/store_mall_directory/527FFF" alt="Store Icon" className="large-icon" />
-              Markets
-            </h2>
-            {data.listMarkets.items.map(market => (
-              <div key={market.id} className="my-2">
-                <Card 
-                  bodyStyle={{
-                    padding: '0.7em',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'space-between'
-                  }}
-                >
-                  <div>
-                    <span className="flex">
-                      <Link className="link" to={`/markets/${market.id}`}>
-                        {market.name}
-                      </Link>
-                      <span style={{ color: 'var(--darkAmazonOrange)' }}>
-                        {market.products.items.length}
-                      </span>
-                      <img src="https://icon.now.sh/shopping_cart/f60" alt="shopping cart" />
-                    </span>
-                    <div style={{ color: 'var(--lightSquidInk)' }}>
-                      {market.owner}
-                    </div>
-                  </div>
-                  <div>
-                    {market.tags && market.tags.map(tag => (
-                      <Tag key={tag} type="danger" className="mx-1">
-                        {tag}
-                      </Tag>
-                    ))}
-                  </div>
-                </Card>
-              </div>
-            ))}
-          </>
-        )
-      }}
-    </Connect>
-  );
+				return (
+					<>
+						<h2 className='header'>
+							<img
+								src='https://icon.now.sh/store_mall_directory/527FFF'
+								alt='Store Icon'
+								className='large-icon'
+							/>
+							Markets
+						</h2>
+						{data.listMarkets.items.map(market => (
+							<div key={market.id} className='my-2'>
+								<Card
+									bodyStyle={{
+										padding: '0.7em',
+										display: 'flex',
+										alignItems: 'center',
+										justifyContent: 'space-between',
+									}}
+								>
+									<div>
+										<span className='flex'>
+											<Link className='link' to={`/markets/${market.id}`}>
+												{market.name}
+											</Link>
+											<span style={{color: 'var(--darkAmazonOrange)'}}>
+												{market.products.items.length}
+											</span>
+											<img
+												src='https://icon.now.sh/shopping_cart/f60'
+												alt='shopping cart'
+											/>
+										</span>
+										<div style={{color: 'var(--lightSquidInk)'}}>
+											{market.owner}
+										</div>
+									</div>
+									<div>
+										{market.tags &&
+											market.tags.map(tag => (
+												<Tag key={tag} type='danger' className='mx-1'>
+													{tag}
+												</Tag>
+											))}
+									</div>
+								</Card>
+							</div>
+						))}
+					</>
+				);
+			}}
+		</Connect>
+	);
 }
 
 export default MarketList;
